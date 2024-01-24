600697033718d83f39ae94271cb12a75e5120bad
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-01-24 12:24:33 -0500

    again

--- routes/clients.js
+++
@@ -1,5 +0,0 @@
-const clientsController = require('../controllers/clientsController');
-module.exports = (app)=>{
-    app.route('/clients').get(clientsController.getClients)
-    app.route('/client').post(clientsController.createClient)
-}
