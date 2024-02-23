7a6b2ce063205197257e71cb4c15e15b642e79f8
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-02-22 22:26:20 -0500

    last

--- public/voz.js
+++ public/voz.js
@@ -13,7 +13,7 @@ vozTexto=(tipoExamen)=>{
 
     recognition.onresult = (event) => {
         const texto = event.results [event.results.length - 1][0].transcript;
-        alert(texto);
+        //alert(texto);
 
         digitarCalificaciones(texto, tipoExamen);
 
@@ -47,8 +47,25 @@ function cadenaArreglo(cadena) {
 
     // Convertir cada substring en un número entero y almacenarlo en un nuevo arreglo
     var arregloDeEnteros = numerosComoTexto.map(function(numero) {
-        return parseInt(numero, 10);
+        var numeroEntero = parseInt(numero, 10);
+        // Verificar si el número está dentro del intervalo o es un NaN
+        if (isNaN(numeroEntero) || numeroEntero < 0 || numeroEntero > 20) {
+            leerTexto('valores fuera de rango')
+            return null; // Almacenar null si no cumple con las condiciones
+
+        } else {
+            return numeroEntero; // Almacenar el número entero si cumple con las condiciones
+        }
     });
 
     return arregloDeEnteros;
+}
+
+function leerTexto (text) {
+    const speech = new SpeechSynthesisUtterance(text);
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    speech.lang = 'es-ES'
+    window.speechSynthesis.speak(speech);
 }
